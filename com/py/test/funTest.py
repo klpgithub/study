4e27e09720b447812a3eac9a98cb4cba1fd8354f
klpgithub <820195318@qq.com>
2017-05-10 22:18:58 +0800

    修改

--- com/py/test/funTest.py
+++ com/py/test/funTest.py
@@ -4,14 +4,19 @@
 
 
 # 函数
-print(abs(-100))
-
-print(bool(''))
-print(bool('1'))
-print(bool(0))
-a = abs
-print(a(-99))
-
-
+# print(abs(-100))
+#
+# print(bool(''))
+# print(bool('1'))
+# print(bool(0))
+# a = abs
+# print(a(-99))
 
 
+# 自定义函数
+def enroll(name, age):
+    print("name: ", name)
+    print("age :", age)
+
+
+enroll('张三', 20)
